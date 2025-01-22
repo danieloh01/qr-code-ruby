f719b8696d3b2abd82b0ddaebbc7331cadc4b070
danieloh <56809357+danieloh01@users.noreply.github.com>
2025-01-22 21:39:35 +0000

    final commit

--- qr.rb
+++ qr.rb
@@ -1,3 +1,9 @@
-require "qrcode"
+require "rqrcode"
 
-qrcode = 
+qrcode = RQRCode::QRCode.new("https://en.wikipedia.org/wiki/QR_code")
+png = qrcode.as_png({ :size => 500})
+IO.binwrite("sometext.png", png.to_s)
+
+
+
+# WIFI:T:WPA;S:My wifi network;P:supersecretpassword
